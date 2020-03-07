71d78b39f5825e05594c2a2978d1a177e4e4b7ef
web1mhz <web1mhz@gmail.com>
2020-03-07 18:27:47 +0900

    re_path로 urls를 정규식으로 표현

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
-    path(r'', views.post_list, name='post_list'),
+    #path('', views.post_list, name='post_list'),
+    re_path(r'^$', views.post_list, name='post_list'),
 ]
