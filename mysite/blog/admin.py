70fa57a1c8402ebab8eb218e8b5d151dd2619923
web1mhz <web1mhz@gmail.com>
2020-03-07 18:51:07 +0900

    이미지필드생성_pillow설치

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,4 +3,10 @@ from blog.models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+#admin.site.register(Post)
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at']
+    list_display_links=['title']
+
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at=models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    photo=models.ImageField()
+    created_at=models.DateTimeField(auto_now_add=True, verbose_name='등록일')
     updated_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
